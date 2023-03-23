93826704a26f65e4639d01368994df12cadda68c
Mohd Yousuf <yousufulabrarkhan@gmail.com>
2023-03-23 18:36:56 +0530

    Updates

---
+++ precache-manifest.4d06bf6af18c8cf5d037f2053a3d365a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7855b274538d17934721",
+    "url": "/learning-React/static/css/main.aec13372.chunk.css"
+  },
+  {
+    "revision": "7855b274538d17934721",
+    "url": "/learning-React/static/js/main.7855b274.chunk.js"
+  },
+  {
+    "revision": "63886da14744404a074b",
+    "url": "/learning-React/static/js/1.63886da1.chunk.js"
+  },
+  {
+    "revision": "8bd7a85ff3d018ec0df6",
+    "url": "/learning-React/static/js/runtime~main.8bd7a85f.js"
+  },
+  {
+    "revision": "372ad435565f86ac79604d2e70a13a36",
+    "url": "/learning-React/index.html"
+  }
+];
